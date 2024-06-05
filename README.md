beb4dae7ff3f4cd0234a3a49e187c3abc6d655f6
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-06-05 12:58:23 +0100

    Add files via upload
    less VRAM, prompt embeds caching, i2i

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 ## Hunyuan-DiT for webui ##
 ### Forge tested, probably A1111 too ###
 I don't think there is anything Forge specific here.
-### kinda-sorta-works for me <sup>TM</sup> on 8Gb VRAM, 16Gb RAM (GTX1070) ###
+### works for me <sup>TM</sup> on 8Gb VRAM, 16Gb RAM (GTX1070) ###
 
 ---
 ### screenshot ###
 current UI
 
 ![](screenshot.png "UI screenshot")
-
 
 
 ---
@@ -21,12 +20,16 @@ diffusers>=0.28.1
 ```
 
 ---
+### update 05/06/2024 ###
+* reduced VRAM, no longer flirting with shared memory
+* caching of prompt embeds to avoid text encoder processing if prompt and negative not changed
+* img2img, same method as used with PixArt
+image2image progression with a nice denoise
+![](i2i.png "image2image sequence")
 
-Generating with 8GB VRAM is just about possible, but starts using shared memory easily. Therefore: *slow*. Using CFG 1 keeps generation within 8GB VRAM. Using small resolutions (768x768) seems to give very poor results, stay around 1024x1024. Resolution binning is NOT enabled (width/height would be automatically adjusted to supported values) as this seems to cause issues along edges.
+---
 
-I might keep working on this, but probably not. I've tested the usual ways to reduce VRAM usage, to no avail. Also, the results I'm getting are *variable*.
-
-The pipeline has a 'latents' parameter, so img2img should be easy to add.
+Generating with 8GB VRAM is possible. Using CFG 1 saves some VRAM and is considerably faster, but still slower than equivalent resolutions with sdXL or PixArt. Using small resolutions (768x768) seems to give very poor/broken results. Resolution binning is NOT enabled (width/height would be automatically adjusted to 'supported' values) as this seems to cause issues along edges.
 
 ---
 
--- scripts/hunyuan_DiT_diffusers.py
+++ scripts/hunyuan_DiT_diffusers.py
@@ -24,10 +24,14 @@ class HunyuanStorage:
     lastSeed = -1
     lastPrompt = None
     lastNegative = None
-    pos_embeds = None
-    pos_attention = None
-    neg_embeds = None
-    neg_attention = None
+    prompt_embeds = None
+    negative_prompt_embeds = None
+    prompt_attention_mask = None
+    negative_prompt_attention_mask = None
+    prompt_embeds_2 = None
+    negative_prompt_embeds_2 = None
+    prompt_attention_mask_2 = None
+    negative_prompt_attention_mask_2 = None
     karras = False
 
 from transformers import BertModel, BertTokenizer, CLIPImageProcessor, MT5Tokenizer, T5EncoderModel
@@ -62,14 +66,14 @@ def quote(text):
     return json.dumps(text, ensure_ascii=False)
 
 # modules/processing.py
-def create_infotext(positive_prompt, negative_prompt, guidance_scale, steps, seed, scheduler, width, height):
+def create_infotext(positive_prompt, negative_prompt, guidance_scale, guidance_rescale, steps, seed, scheduler, width, height):
     karras = " : Karras" if HunyuanStorage.karras == True else ""
     generation_params = {
         "Size": f"{width}x{height}",
         "Seed": seed,
         "Scheduler": f"{scheduler}{karras}",
         "Steps": steps,
-        "CFG": guidance_scale,
+        "CFG": f"{guidance_scale}({guidance_rescale})",
         "RNG": opts.randn_source if opts.randn_source != "GPU" else None
     }
 
@@ -81,7 +85,7 @@ def create_infotext(positive_prompt, negative_prompt, guidance_scale, steps, see
 
     return f"Model: Hunyuan-DiT\n{prompt_text}{generation_params_text}"
 
-def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num_steps, sampling_seed, num_images, scheduler, style, *args):
+def predict(positive_prompt, negative_prompt, width, height, guidance_scale, guidance_rescale, num_steps, sampling_seed, num_images, scheduler, style, i2iSource, i2iDenoise, *args):
 
     if style != 0:
         positive_prompt = styles.styles_list[style][1].replace("{prompt}", positive_prompt)
@@ -96,86 +100,98 @@ def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num
     fixed_seed = get_fixed_seed(sampling_seed)
     HunyuanStorage.lastSeed = fixed_seed
 
-
 #    algorithm_type = args.algorithm
 #    beta_schedule = args.beta_schedule
 #    use_lu_lambdas = args.use_lu_lambdas
 
-#    useCachedEmbeds = False#(HunyuanStorage.lastPrompt == positive_prompt and HunyuanStorage.lastNegative == negative_prompt)
+    useCachedEmbeds = (HunyuanStorage.lastPrompt == positive_prompt and HunyuanStorage.lastNegative == negative_prompt)
 
 #    logging.set_verbosity(logging.ERROR)
 
-    pipe = HunyuanDiTPipeline.from_pretrained(
-        "Tencent-Hunyuan/HunyuanDiT-Diffusers",
-        requires_safety_checker=False,
-        safety_checker=None,
-        feature_extractor=None, )
+    if useCachedEmbeds:
+        print ("Skipping text encoders and tokenizers.")
+        pipe = HunyuanDiTPipeline.from_pretrained(
+            "Tencent-Hunyuan/HunyuanDiT-Diffusers",
+            requires_safety_checker=False,
+            safety_checker=None,
+            feature_extractor=None,
+            device='cuda',
+            torch_dtype=torch.float16,
+            tokenizer=None,
+            text_encoder=None,
+            tokenizer_2=None,
+            text_encoder_2=None
+            )
+        pipe.enable_model_cpu_offload()
+        prompt_embeds = HunyuanStorage.prompt_embeds.to('cuda')
+        negative_prompt_embeds = HunyuanStorage.negative_prompt_embeds.to('cuda')
+        prompt_attention_mask = HunyuanStorage.prompt_attention_mask.to('cuda')
+        negative_prompt_attention_mask = HunyuanStorage.negative_prompt_attention_mask.to('cuda')
+        prompt_embeds_2 = HunyuanStorage.prompt_embeds_2.to('cuda')
+        negative_prompt_embeds_2 = HunyuanStorage.negative_prompt_embeds_2.to('cuda')
+        prompt_attention_mask_2 = HunyuanStorage.prompt_attention_mask_2.to('cuda')
+        negative_prompt_attention_mask_2 = HunyuanStorage.negative_prompt_attention_mask_2.to('cuda')
+       
+    else:
+        pipe = HunyuanDiTPipeline.from_pretrained(
+            "Tencent-Hunyuan/HunyuanDiT-Diffusers",
+            requires_safety_checker=False,
+            safety_checker=None,
+            feature_extractor=None,
+            device='cuda',
+            torch_dtype=torch.float16,
+            )
+        pipe.enable_model_cpu_offload()
+        with torch.no_grad():
+            (
+                prompt_embeds,
+                negative_prompt_embeds,
+                prompt_attention_mask,
+                negative_prompt_attention_mask,
+            ) = pipe.encode_prompt(
+                prompt=positive_prompt,
+                negative_prompt=negative_prompt,
+                device='cuda',
+                dtype=torch.float16,
+                num_images_per_prompt=num_images,
+                do_classifier_free_guidance=True,
+                max_sequence_length=77,
+                text_encoder_index=0,
+            )
+            (
+                prompt_embeds_2,
+                negative_prompt_embeds_2,
+                prompt_attention_mask_2,
+                negative_prompt_attention_mask_2,
+            ) = pipe.encode_prompt(
+                prompt=positive_prompt,
+                negative_prompt=negative_prompt,
+                device='cuda',
+                dtype=torch.float16,
+                num_images_per_prompt=num_images,
+                do_classifier_free_guidance=True,
+                max_sequence_length=256,
+                text_encoder_index=1,
+            )
+        HunyuanStorage.prompt_embeds = prompt_embeds.to('cpu')
+        HunyuanStorage.negative_prompt_embeds = negative_prompt_embeds.to('cpu')
+        HunyuanStorage.prompt_attention_mask = prompt_attention_mask.to('cpu')
+        HunyuanStorage.negative_prompt_attention_mask = negative_prompt_attention_mask.to('cpu')
+        HunyuanStorage.prompt_embeds_2 = prompt_embeds_2.to('cpu')
+        HunyuanStorage.negative_prompt_embeds_2 = negative_prompt_embeds_2.to('cpu')
+        HunyuanStorage.prompt_attention_mask_2 = prompt_attention_mask_2.to('cpu')
+        HunyuanStorage.negative_prompt_attention_mask_2 = negative_prompt_attention_mask_2.to('cpu')
+        HunyuanStorage.lastPrompt = positive_prompt
+        HunyuanStorage.lastNegative = negative_prompt
 
-#    del pipe.vae            #stupid implementation needs transformer and vae during pipe init
-#    del pipe.transformer
+        del pipe.tokenizer, pipe.tokenizer_2, pipe.text_encoder, pipe.text_encoder_2
+        pipe.tokenizer = None
+        pipe.tokenizer_2 = None
+        pipe.text_encoder = None
+        pipe.text_encoder_2 = None
 
-
-    (
-        prompt_embeds,
-        negative_prompt_embeds,
-        prompt_attention_mask,
-        negative_prompt_attention_mask,
-    ) = pipe.encode_prompt(
-        prompt=positive_prompt,
-        negative_prompt=negative_prompt,
-        device='cpu',
-        dtype=torch.float16,
-        num_images_per_prompt=num_images,
-        do_classifier_free_guidance=True,
-##            prompt_embeds=prompt_embeds,
-##            negative_prompt_embeds=negative_prompt_embeds,
-##            prompt_attention_mask=prompt_attention_mask,
-##            negative_prompt_attention_mask=negative_prompt_attention_mask,
-        max_sequence_length=77,
-        text_encoder_index=0,
-    )
-    (
-        prompt_embeds_2,
-        negative_prompt_embeds_2,
-        prompt_attention_mask_2,
-        negative_prompt_attention_mask_2,
-    ) = pipe.encode_prompt(
-        prompt=positive_prompt,
-        negative_prompt=negative_prompt,
-        device='cpu',
-        dtype=torch.float16,
-        num_images_per_prompt=num_images,
-        do_classifier_free_guidance=True,
-##            prompt_embeds=prompt_embeds_2,
-##            negative_prompt_embeds=negative_prompt_embeds_2,
-##            prompt_attention_mask=prompt_attention_mask_2,
-##            negative_prompt_attention_mask=negative_prompt_attention_mask_2,
-        max_sequence_length=256,
-        text_encoder_index=1,
-    )
-
-
-    del pipe.tokenizer, pipe.tokenizer_2, pipe.text_encoder, pipe.text_encoder_2
-    pipe.tokenizer = None
-    pipe.tokenizer_2 = None
-    pipe.text_encoder = None
-    pipe.text_encoder_2 = None
-
-    gc.collect()
-    torch.cuda.empty_cache()
-
-##    pipe = HunyuanDiTPipeline.from_pretrained(
-##        "Tencent-Hunyuan/HunyuanDiT-Diffusers",
-##        requires_safety_checker=False,
-##        safety_checker=None,
-##        feature_extractor=None,
-##        tokenizer=None,
-##        text_encoder=None,
-##        tokenizer_2=None,
-##        text_encoder_2=None,)
-
-    pipe.to('cuda')
-    pipe.enable_model_cpu_offload()
+        gc.collect()
+        torch.cuda.empty_cache()
 
     pipe.vae.enable_tiling(True)
 #    pipe.enable_attention_slicing()
@@ -184,7 +200,29 @@ def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num
         #   if using resolution_binning, must use adjusted width/height here (don't overwrite values)
         #   always generate the noise here
         generator = [torch.Generator(device='cpu').manual_seed(fixed_seed+i) for i in range(num_images)]
+        shape = (
+            num_images,
+            pipe.transformer.config.in_channels,
+            int(height) // pipe.vae_scale_factor,
+            int(width) // pipe.vae_scale_factor,
+        )
 
+        i2i_latents = randn_tensor(shape, generator=generator, dtype=torch.float16).to('cuda').to(torch.float16)
+
+        if i2iSource != None:
+            i2iSource = i2iSource.resize((width, height))
+
+            image = pipe.image_processor.preprocess(i2iSource).to('cuda').to(torch.float16)
+            image_latents = pipe.vae.encode(image).latent_dist.sample(generator) * pipe.vae.config.scaling_factor * pipe.scheduler.init_noise_sigma
+            image_latents = image_latents.repeat(num_images, 1, 1, 1)
+
+            pipe.scheduler = DDPMScheduler.from_config(pipe.scheduler.config)
+            ts = torch.tensor([int(1000 * i2iDenoise) - 1], device='cpu')
+            ts = ts[:1].repeat(num_images)
+
+            i2i_latents = pipe.scheduler.add_noise(image_latents, i2i_latents, ts)
+
+            del image, image_latents, i2iSource
     
     if scheduler == 'DDPM':
         pipe.scheduler = DDPMScheduler.from_config(pipe.scheduler.config)
@@ -208,6 +246,7 @@ def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num
         pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 #   else uses default set by model
 
+    pipe.scheduler.config.num_train_timesteps = int(1000 * i2iDenoise)
     pipe.scheduler.config.use_karras_sigmas = HunyuanStorage.karras
 
 ##    pipe.scheduler.beta_schedule  = beta_schedule
@@ -220,21 +259,23 @@ def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num
         height=height,
         width=width,
         guidance_scale=guidance_scale,
-        prompt_embeds=prompt_embeds.to('cuda').to(torch.float16),
-        negative_prompt_embeds=negative_prompt_embeds.to('cuda').to(torch.float16),
-        prompt_attention_mask=prompt_attention_mask.to('cuda').to(torch.float16),
-        negative_prompt_attention_mask=negative_prompt_attention_mask.to('cuda').to(torch.float16),
-        prompt_embeds_2=prompt_embeds_2.to('cuda').to(torch.float16),
-        negative_prompt_embeds_2=negative_prompt_embeds_2.to('cuda').to(torch.float16),
-        prompt_attention_mask_2=prompt_attention_mask_2.to('cuda').to(torch.float16),
-        negative_prompt_attention_mask_2=negative_prompt_attention_mask_2.to('cuda').to(torch.float16),
+#        guidance_rescale=guidance_rescale,
+        prompt_embeds=prompt_embeds,
+        negative_prompt_embeds=negative_prompt_embeds,
+        prompt_attention_mask=prompt_attention_mask,
+        negative_prompt_attention_mask=negative_prompt_attention_mask,
+        prompt_embeds_2=prompt_embeds_2,
+        negative_prompt_embeds_2=negative_prompt_embeds_2,
+        prompt_attention_mask_2=prompt_attention_mask_2,
+        negative_prompt_attention_mask_2=negative_prompt_attention_mask_2,
         num_images_per_prompt=num_images,
         output_type="pil",
         generator=generator,
         use_resolution_binning=False,
+        latents=i2i_latents,
     ).images
 
-    del pipe
+    del pipe, generator, i2i_latents
     gc.collect()
     torch.cuda.empty_cache()
 
@@ -243,7 +284,7 @@ def predict(positive_prompt, negative_prompt, width, height, guidance_scale, num
     for image in output:
         info=create_infotext(
             positive_prompt, negative_prompt,
-            guidance_scale, num_steps, 
+            guidance_scale, guidance_rescale, num_steps, 
             fixed_seed, scheduler,
             width, height, )
 
@@ -278,6 +319,19 @@ def on_ui_tabs():
     def randomSeed ():
         return -1
 
+    def i2iSetDimensions (image, w, h):
+        if image is not None:
+            w = 32 * (image.size[0] // 32)
+            h = 32 * (image.size[1] // 32)
+        return [w, h]
+
+    def i2iImageFromGallery (gallery):
+        try:
+            newImage = gallery[HunyuanStorage.galleryIndex][0]['name'].split('?')
+            return newImage[0]
+        except:
+            return None
+
     def toggleKarras ():
         if HunyuanStorage.karras == False:
             HunyuanStorage.karras = True
@@ -294,9 +348,8 @@ def on_ui_tabs():
         with ResizeHandleRow():
             with gr.Column():
                 with gr.Row():
-                    positive_prompt = gr.Textbox(label='Prompt', placeholder='Enter a prompt here...', default='', lines=2)
-                    scheduler = gr.Dropdown(["default",
-                                             "DDPM",
+                    positive_prompt = gr.Textbox(label='Prompt', placeholder='Enter a prompt here...', default='', lines=1.1)
+                    scheduler = gr.Dropdown(["DDPM",
                                              "DEIS",
                                              "DPM++ 2M",
                                              "DPM++ 2M SDE",
@@ -312,7 +365,7 @@ def on_ui_tabs():
 
 
                 with gr.Row():
-                    negative_prompt = gr.Textbox(label='Negative', placeholder='', lines=2)
+                    negative_prompt = gr.Textbox(label='Negative', placeholder='', lines=1.1)
                     style = gr.Dropdown([x[0] for x in styles.styles_list], label='Style', value="(None)", type='index', scale=0)
                 with gr.Row():
                     width = gr.Slider(label='Width', minimum=512, maximum=1536, step=32, value=1024, elem_id="Hunyuan-DiT_width")
@@ -321,14 +374,24 @@ def on_ui_tabs():
 
                 with gr.Row():
                     guidance_scale = gr.Slider(label='CFG', minimum=1, maximum=16, step=0.5, value=1, scale=2)
-                    steps = gr.Slider(label='Steps', minimum=1, maximum=80, step=1, value=20, scale=2)
+                    guidance_rescale = gr.Slider(label='rescale CFG', minimum=0, maximum=16, step=1, value=0, scale=2)
                 with gr.Row():
+                    steps = gr.Slider(label='Steps', minimum=1, maximum=80, step=1, value=20, scale=2)
                     sampling_seed = gr.Number(label='Seed', value=-1, precision=0, scale=1)
                     random = ToolButton(value="\U0001f3b2\ufe0f")
                     reuseSeed = ToolButton(value="\u267b\ufe0f")
                     batch_size = gr.Number(label='Batch Size', minimum=1, maximum=9, value=1, precision=0, scale=0)
 
-                ctrls = [positive_prompt, negative_prompt, width, height, guidance_scale, steps, sampling_seed, batch_size, scheduler, style]
+                with gr.Accordion(label='image to image', open=False):
+                    with gr.Row():
+                        i2iSource = gr.Image(label='image source', sources=['upload'], type='pil', interactive=True, show_download_button=False)
+                        with gr.Column():
+                            i2iDenoise = gr.Slider(label='Denoise', minimum=0.00, maximum=1.0, step=0.01, value=0.5)
+                            i2iSetWH = gr.Button(value='Set safe Width / Height from image')
+                            i2iFromGallery = gr.Button(value='Get image from gallery')
+
+                ctrls = [positive_prompt, negative_prompt, width, height, guidance_scale, guidance_rescale, steps, sampling_seed,
+                         batch_size, scheduler, style, i2iSource, i2iDenoise]
 
             with gr.Column():
                 generate_button = gr.Button(value="Generate", variant='primary', visible=True)
@@ -352,6 +415,9 @@ def on_ui_tabs():
         swapper.click(fn=None, _js="function(){switchWidthHeight('Hunyuan-DiT')}", inputs=None, outputs=None, show_progress=False)
         random.click(randomSeed, inputs=[], outputs=sampling_seed, show_progress=False)
         reuseSeed.click(reuseLastSeed, inputs=[], outputs=sampling_seed, show_progress=False)
+
+        i2iSetWH.click (fn=i2iSetDimensions, inputs=[i2iSource, width, height], outputs=[width, height], show_progress=False)
+        i2iFromGallery.click (fn=i2iImageFromGallery, inputs=[output_gallery], outputs=[i2iSource])
 
         output_gallery.select (fn=getGalleryIndex, inputs=[], outputs=[])
 
